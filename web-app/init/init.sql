68bbb3f298ed397e6b41218fb904bce95f11cd49
unknown <prajwalrao0404@gmail.com>
2024-07-25 14:05:24 +0530

    Initial upload

---
+++ web-app/init/init.sql
@@ -0,0 +1,16 @@
+#Initialization Script for MySQL
+
+CREATE TABLE IF NOT EXISTS task (
+    task_id int(10) PRIMARY KEY,
+    task varchar(250) NOT NULL,
+    status varchar(30) NOT NULL
+);
+
+INSERT INTO task (task_id, task, status)
+VALUES
+    (1, 'Read an article on React.js', 'Done'),
+    (2, 'Organize a meeting', 'Pending')
+ON DUPLICATE KEY UPDATE task = VALUES(task), status = VALUES(status);
+
+ALTER TABLE task
+    MODIFY task_id int(10) AUTO_INCREMENT, AUTO_INCREMENT=3;
